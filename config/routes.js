ba06dd16340cb7a178fa695894f00893799a9083
La Baik <labaik237@gmail.com>
2016-02-26 20:58:14 -0800

    trying to fix cors issue

--- config/routes.js
+++ config/routes.js
@@ -7,6 +7,11 @@ var cors             = require('cors')
 
 // allow cross origin access
 app.use(cors());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 //
 // REQUIRE CONTROLLERS
